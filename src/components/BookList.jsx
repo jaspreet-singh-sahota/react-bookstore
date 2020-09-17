079f77f0b0f7d7162e90f37f1ee545d71af624b9
OdongAlican <sandieo.2020@gmail.com>
2020-09-17 16:31:02 +0300

    add book table and connect redux

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
 
-function BookList() {
-  return (
-    <div>
-      
-    </div>
-  )
+import React from 'react'
+import { connect } from 'react-redux'
+import Book from '../containers/Book'
+
+const BooksList = ({ books }) => {
+
+
+    return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Book ID</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                    </tr>
+                </thead>
+               <tbody>
+                   {
+                       books.map( book => (
+                        <Book book = {book} key={book.id}/>
+                       ))
+                   }
+                </tbody> 
+            </table>
+        </div>
+    )
 }
 
-export default BookList
+const mapStateToProps = state => ({
+    books: state.booksReducer,
+})
+
+const BooksListConnect = connect(mapStateToProps, null)(BooksList)
+
+export default BooksListConnect;
