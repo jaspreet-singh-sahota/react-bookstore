451ab6322d86f6a5b9bdfffc0e840c0ce350d8c1
OdongAlican <sandieo.2020@gmail.com>
2020-09-17 16:32:45 +0300

    add single book row

---
+++ src/containers/Book.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Book = ({ book }) => {
+    return (
+              <tr>
+                <td>{book.id}</td>
+                <td>{book.title}</td>
+                <td>{book.category}</td>
+            </tr>
+    )
+}
+
+export default Book
