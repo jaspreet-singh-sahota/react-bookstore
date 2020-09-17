2b0f3bcf34efc007cd3036de3cdc2f5af82cfe68
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-17 15:08:39 +0530

    Add BookList and BooksForm components to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import BookList from './components/BookList';
+import BooksForm from './components/BooksForm';
 
 function App() {
   return (
     <div>
       <h1 className="title">Book store</h1>
+      <BookList/>
+      <BooksForm/>
     </div>
   );
 }
