ee9528de047ad6b3a7a43528f4b97128aba0474f
OdongAlican <sandieo.2020@gmail.com>
2020-09-17 16:34:12 +0300

    add combineReducers for bookReduder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import booksReducer from './books';
 
-const rootReducer = combineReducers ({
-  
-})
+const rootReducer = combineReducers({
+  booksReducer,
+});
+
+export default rootReducer;
