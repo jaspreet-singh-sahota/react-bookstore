3f7dcebea2aec0771f25319fad4180abf5315b4c
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-18 19:12:00 +0530

    Add categoryFilter component

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { categories } from "../reducers/categoryData";
+
+const CategoryFilter = ({ filterCategory }) => {
+
+  const handleFilter = ({ target }) => {
+    filterCategory(target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="category">CATEGORIES</label>
+      <select name="category" onChange={handleFilter}>
+        <option value="All" key="All">
+          All
+        </option>
+        {categories.map(category => (
+          <option value={category} key={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+CategoryFilter.propTypes = {
+  filterCategory: PropTypes.func.isRequired,
+};
+
+export default CategoryFilter;
