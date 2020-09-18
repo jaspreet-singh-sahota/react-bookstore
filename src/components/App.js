bed27b8bcf71cc1fcc64d53894f92f40af50c752
OdongAlican <sandieo.2020@gmail.com>
2020-09-18 20:54:51 +0300

    Add the following to Navbar - Add nav-bar components - Add respective styles to navbar components

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,22 @@ import BooksForm from '../containers/BooksForm';
 function App() {
   return (
     <div>
-      <h1 className="title">Book store</h1>
+      <div className="nav-bar">
+        <div className="bookstore-title">
+          <h1>Bookstore CMS</h1>
+        </div>
+        <div className="nav-name">
+          <p>BOOKS</p>
+        </div>
+        <div className="cat-name">
+          <p>CATEGORIES</p>
+        </div>
+        <div className="profile-name">
+          <div className="border-section">
+            <i className="fas fa-user" />
+          </div>
+        </div>
+      </div>
       <BookList />
       <BooksForm />
     </div>
