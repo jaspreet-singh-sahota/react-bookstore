0182007ebab55f8158ddab29bf82e17426402457
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-18 19:04:42 +0530

    Add changeFilter action creator function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,17 @@
+import { DELETE_BOOK, CHANGE_FILTER, CREATE_BOOK } from "../../../bookstore/src/actions/actionType";
+
 export const createBook = book => ({
-  type: 'CREATE_BOOK',
+  type: CREATE_BOOK,
   payload: book,
 });
 
 export const deleteBook = book => ({
-  type: 'DELETE_BOOK',
+  type: DELETE_BOOK,
   payload: book.id,
 });
+
+export const changeFilter = category => ({
+  type: CHANGE_FILTER,
+  payload: category,
+});
+
