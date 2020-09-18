0b50a7b888bded9ab71cdc4059a3303427fca197
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-18 13:38:39 +0530

    Change the bookform component to the stateful component

--- src/components/BooksForm.jsx
+++ src/components/BooksForm.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 
-const BooksForm = () => {
+import React, { Component } from 'react'
 
+class BooksForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: '',
+      category: ''
+    }
+  }
+  render() {
     const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
 
-    return(
-       <form>
-           <input placeholder="Enter Book Title" type="text"/>
-           <select>
-               { categories.map(category => (
-                   <option key={category}>{ category }</option>
-               ))}
-            </select> 
-            <button type="submit">Submit</button>
-       </form> 
+    return (
+      <form>
+        <input placeholder="Enter Book Title" type="text" />
+        <select>
+          {categories.map(category => (
+            <option key={category}>{category}</option>
+          ))}
+        </select>
+        <button type="submit">Submit</button>
+      </form>
     )
+  }
 }
+
+
 
 
 export default BooksForm;
