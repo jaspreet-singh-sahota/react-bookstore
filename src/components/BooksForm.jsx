909881d6017a3418cdfba008a63967cf14d0607c
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-18 13:42:46 +0530

    Upadted form with the event listeners

--- src/components/BooksForm.jsx
+++ src/components/BooksForm.jsx
@@ -11,13 +11,18 @@ class BooksForm extends Component {
       category: ''
     }
   }
+
+  
+
   render() {
     const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
 
     return (
-      <form>
-        <input placeholder="Enter Book Title" type="text" />
-        <select>
+      <form onClick={handleSubmit} onChange={handleChange}>
+        <label htmlFor="title">Title</label>
+        <input name="title" placeholder="Enter Book Title" type="text" onChange={handleChange} />
+        <label htmlFor="category">Category</label>
+        <select name="category">
           {categories.map(category => (
             <option key={category}>{category}</option>
           ))}
