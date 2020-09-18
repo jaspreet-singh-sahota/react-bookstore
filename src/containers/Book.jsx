7cbf49e2e8239a6558dda32c29bbfddf2b58aa5b
OdongAlican <sandieo.2020@gmail.com>
2020-09-18 11:57:34 +0300

    remove book functionality

--- src/containers/Book.jsx
+++ src/containers/Book.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Book = ({ book }) => {
+const Book = ({ book, removeBook }) => {
     return (
               <tr>
                 <td>{book.id}</td>
                 <td>{book.title}</td>
                 <td>{book.category}</td>
+                <td>
+                    <button onClick={()=>removeBook(book)}>Remove Book</button>
+                </td>
             </tr>
     )
 }
